0f6351ccbe321c0dcc4b2972a3156b115f2c95ca
Tsui Yik Ching <tsuiyikching@protonmail.com>
2020-03-12 16:32:27 +0000

    Tests for sorter(), election() and loop_elections()

--- testing.jl
+++ testing.jl
@@ -1,4 +1,15 @@
 module testing
+#=
+Unit tests require Test and InteractiveUtils to be installed. The latter doesn't
+need to be explicitly using'ed if you're running the tests the Jupyter notebook
+
+1. Move elections.jl and testing.jl to the same folder
+2. Inside Julia (REPL), type push!(LOAD_PATH, "path_of_your_folder"). You may 
+   want to add it to your startup.jl as well
+3. Type (in Julia REPL), include("path_of_your_folder/testing.jl"). The tests
+   will immediately run
+=#
+
 using elections
 using Test
 using InteractiveUtils
@@ -11,6 +22,11 @@ test_distance_list = [[1 2 3];     # 1 2 3
                       [2 1 3];     # 2 1 3
                       [2 3 1];     # 3 1 2
                       [3 2 1];]    # 3 2 1
+
+@testset "sorter" begin
+    @test sorter(test_distance_list) == [1 2 3; 2 1 3; 3 1 2; 3 2 1]
+    @inferred sorter(test_distance_list)
+end
 
 @testset "FPTP" begin
     @test fptp(test_distance_list) ≈ 3
@@ -37,17 +53,14 @@ end
 end
 
 @testset "election functions" begin
+    @test typeof(election(1, 1, 1, 100, [1 2 3], [3 2 1])) == NTuple{5,Int64}
     @inferred election(1, 1, 1, 100, [1 2 3], [3 2 1])
+    @test size(loop_elections(Iterators.product(-2:1:2, -2:1:2), [1 2 3], [3 2 1])) == (25, 7)
     @inferred loop_elections(Iterators.product(-2:1:2, -2:1:2), [1 2 3], [3 2 1])
 end
 
-@testset "unfold" begin
-    @test unfold!(sortperm.(eachrow(test_distance_list))) == [[1 2 3]; [1 2 3]; [1 2 3]; [1 2 3]]
-    @inferred unfold!(sortperm.(eachrow(test_distance_list))) == [[1 2 3]; [1 2 3]; [1 2 3]; [1 2 3]]
-end
-
 @code_warntype borda(test_distance_list)
-# and irv shows yellow on Union{Nothing, ...}
-# This is just the loop over the iterator, which returns the next value, or nothing if at end
-
+# this, irv(), and sorted() shows yellow on Union{Nothing, ...}
+# This is just the loop over the iterator, which returns the next value, or 
+# nothing if at end
 end
